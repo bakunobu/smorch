07fec5f68db17191be97937db265e97d9b6b9494
Sergei Vakunov <bakunobu@gmail.com>
2026-07-04 10:03:48 +0300

    migrate tool added

--- src/database.py
+++ src/database.py
@@ -1,10 +1,13 @@
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def init_db(app):
     """Initialize the database with the given Flask app."""
     db.init_app(app)
+    migrate.init_app(app, db)
     with app.app_context():
         db.create_all()
